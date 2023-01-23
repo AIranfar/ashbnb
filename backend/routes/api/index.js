6c93f09114a24b11608030aeb743ba3b0738c6f3
Ashkaun Iranfar <ashkauniranfar@gmail.com>
2023-01-23 14:28:30 -0800

    phase-5-complete

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -13,32 +13,5 @@ router.use('/users', usersRouter);
 //   res.json({ requestBody: req.body });
 // });
 
-// const { setTokenCookie } = require('../../utils/auth.js');
-// const { User } = require('../../db/models');
-// router.get('/set-token-cookie', async (_req, res) => {
-//   const user = await User.findOne({
-//     where: {
-//       username: 'Demo-lition'
-//     }
-//   });
-//   setTokenCookie(res, user);
-//   return res.json({ user: user });
-// });
-
-// router.get(
-//   '/restore-user',
-//   (req, res) => {
-//     return res.json(req.user);
-//   }
-// );
-
-// const { requireAuth } = require('../../utils/auth.js');
-// router.get(
-//   '/require-auth',
-//   requireAuth,
-//   (req, res) => {
-//     return res.json(req.user);
-//   }
-// );
 
 module.exports = router;
