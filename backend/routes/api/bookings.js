f1f4f51fe77346f1783c4302d2c2ac6727ee5411
Ashkaun Iranfar <ashkauniranfar@gmail.com>
2023-01-26 15:39:20 -0800

    delete booking completed and tested locally

--- backend/routes/api/bookings.js
+++ backend/routes/api/bookings.js
@@ -1,7 +1,32 @@
 const express = require('express');
 const router = express.Router();
-const { setTokenCookie, requireAuth } = require('../../utils/auth.js')
+const { requireAuth } = require('../../utils/auth.js')
 const { Booking, Review, ReviewImage, Spot, SpotImage, User } = require('../../db/models');
 
+// Delete a Booking
+
+router.delete('/:bookingId', requireAuth, async (req, res) => {
+    const deletedBooking = await Booking.findByPk(req.params.bookingId);
+
+    if (!deletedBooking) {
+        return res.status(404).json({
+            "message": "Booking couldn't be found",
+            "statusCode": res.statusCode
+        })
+    }
+
+    if (req.user.id !== deletedBooking.userId){
+        return res.status(403).json({
+            "message": "Forbidden",
+            "statusCode": res.statusCode
+        })
+    }
+
+    await deletedBooking.destroy();
+    return res.status(200).json({
+        "message": "Successfully deleted",
+        "statusCode": res.statusCode
+    })
+})
 
 module.exports = router;
