f77f7e71b6dc3d43d461bf0870059070fb1504ab
Ashkaun Iranfar <ashkauniranfar@gmail.com>
2023-01-26 10:46:12 -0800

    delete reviews completed and tested locally

--- backend/routes/api/reviews.js
+++ backend/routes/api/reviews.js
@@ -5,6 +5,22 @@ const { Booking, Review, ReviewImage, Spot, SpotImage, User } = require('../../d
 
 // Delete Reviews
 
+router.delete('/:reviewId', requireAuth, async (req, res) => {
+    const deletedReview = await Review.findByPk(req.params.reviewId);
+
+    if (!deletedReview) {
+        return res.status(404).json({
+            "message": "Review couldn't be found",
+            "statusCode": res.statusCode
+        })
+    }
+
+    await deletedReview.destroy();
+    return res.status(200).json({
+        "message": "Successfully deleted",
+        "statusCode": res.statusCode
+    })
+})
 
 
 module.exports = router;
