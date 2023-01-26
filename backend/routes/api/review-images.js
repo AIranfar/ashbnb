69a1fdab200d5528a7f5f40c22be53c3732c3efc
Ashkaun Iranfar <ashkauniranfar@gmail.com>
2023-01-25 20:52:35 -0800

    wednesday finished

--- backend/routes/api/review-images.js
+++ backend/routes/api/review-images.js
@@ -1,22 +1,24 @@
 const express = require('express');
 const router = express.Router();
 const { requireAuth } = require('../../utils/auth')
-const { ReviewImage } = require('../../db/models')
+const { Booking, Review, ReviewImage, Spot, SpotImage, User } = require('../../db/models');
 
 // Delete Review Image
 
 router.delete('/:imageId', requireAuth, async(req, res, next) => {
     const deletedImage = await ReviewImage.findByPk(req.params.imageId);
 
+    if (!deletedImage) {
+        return res.status(404).json({
+            "message": "Review Image couldn't be found",
+            "statusCode": res.statusCode
+        })
+    }
+
     if (deletedImage) {
         await deletedImage.destroy();
         return res.status(200).json({
             "message": "Successfully deleted",
-            "statusCode": res.statusCode
-        })
-    } else if (!deletedImage) {
-        return res.status(404).json({
-            "message": "Review Image couldn't be found",
             "statusCode": res.statusCode
         })
     }
--- backend/routes/api/reviews.js
+++ backend/routes/api/reviews.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const { setTokenCookie, requireAuth } = require('../../utils/auth')
+const { Booking, Review, ReviewImage, Spot, SpotImage, User } = require('../../db/models');
+
+// Delete Reviews
+
 
 
 module.exports = router;
