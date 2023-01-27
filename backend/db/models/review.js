504bf7c8061a209a964ab5f23cc7d04a0672ca45
Ashkaun Iranfar <ashkauniranfar@gmail.com>
2023-01-27 10:42:44 -0800

    updated model error

--- backend/db/models/review.js
+++ backend/db/models/review.js
@@ -12,7 +12,7 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       Review.hasMany(models.ReviewImage, { foreignKey: 'reviewId' })
       Review.belongsTo(models.User, { foreignKey: 'userId' })
-      Review.belongsTo(models.Spot, { forgeignKey: 'spotId' })
+      Review.belongsTo(models.Spot, { foreignKey: 'spotId' })
     }
   }
   Review.init({
