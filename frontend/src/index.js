cf3c4221edd45903cc458343f85421f07451a7e3
Ashkaun Iranfar <ashkauniranfar@gmail.com>
2023-03-12 14:48:10 -0700

    login modals completed

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,18 +1,20 @@
-// frontend/src/index.js
 import React from 'react';
+
 import './index.css';
-import * as sessionActions from './store/session';
+
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
+import { ModalProvider, Modal } from './context/Modal';
 import App from './App';
+
 import configureStore from './store';
-import { restoreCSRF } from './store/csrf';
-import { csrfFetch } from './store/csrf';
+import { restoreCSRF, csrfFetch } from "./store/csrf";
+import * as sessionActions from './store/session';
 
 const store = configureStore();
 
-if (process.env.NODE_ENV !== 'production') {
+if (process.env.NODE_ENV !== "production") {
   restoreCSRF();
 
   window.csrfFetch = csrfFetch;
@@ -22,11 +24,14 @@ if (process.env.NODE_ENV !== 'production') {
 
 function Root() {
   return (
-    <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </Provider>
+    <ModalProvider>
+      <Provider store={store}>
+        <BrowserRouter>
+          <App />
+          <Modal />
+        </BrowserRouter>
+      </Provider>
+    </ModalProvider>
   );
 }
 
@@ -34,17 +39,5 @@ ReactDOM.render(
   <React.StrictMode>
     <Root />
   </React.StrictMode>,
-  document.getElementById('root'),
+  document.getElementById('root')
 );
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
