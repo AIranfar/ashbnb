297dae149da2cbc974ec4109fb15386534d7fd4f
Ashkaun Iranfar <ashkauniranfar@gmail.com>
2023-03-15 16:56:06 -0700

    delete spot added

--- frontend/src/components/CreateASpot/index.js
+++ frontend/src/components/CreateASpot/index.js
@@ -19,6 +19,7 @@ const CreateASpot = () => {
     const [imageUrl3, setImageUrl3] = useState('');
 
     const handleSubmit = (e) => {
+        e.preventDefault();
     }
 
     return (
--- frontend/src/store/spots.js
+++ frontend/src/store/spots.js
@@ -2,7 +2,8 @@ import { csrfFetch } from './csrf';
 
 const ALL = 'spots/ALL_SPOTS';
 const ONE = 'spots/ONE_SPOT';
-const CREATE = 'spots/CREATE_SPOT'
+const CREATE = 'spots/CREATE_SPOT';
+const DELETE = 'spots/DELETE_SPOT';
 
 const all = list => ({
     type: ALL,
@@ -12,10 +13,15 @@ const all = list => ({
 const one = list => ({
     type: ONE,
     list
-})
+});
 
 const create = list => ({
     type: CREATE,
+    list
+});
+
+const deleteSpot = list => ({
+    type: DELETE,
     list
 })
 
@@ -50,13 +56,23 @@ export const getOneSpot = (spotId) => async dispatch => {
 
 export const createASpot = (spots, spotImages) => async dispatch => {
     const response = await fetch('/api/spots/new', {
-        'method': 'POST',
+        method: 'POST',
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(createASpot),
     });
     if (response.ok) {
 
     }
+}
+
+export const removeSpot = (spotId) => async dispatch => {
+    const response = await csrfFetch(`/api/spots/${spotId}`, {
+        method: 'DELETE'
+    });
+    if (response.ok) {
+        dispatch(deleteSpot(spotId))
+    }
+    return response;
 }
 
     const initialState = {
@@ -77,6 +93,10 @@ export const createASpot = (spots, spotImages) => async dispatch => {
                 const newState = { ...state };
                 newState.singleSpot = action.list;
                 return newState;
+            case DELETE:
+                const newState2 = { ...state, oneSpot: {} };
+                delete newState2.allSpots[action.spotId];
+                return newState2;
             default:
                 return state;
         }
