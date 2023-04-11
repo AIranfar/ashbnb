b73d966d0f6b38ed9b14e6cdb726361514b0224d
Ashkaun Iranfar <ashkauniranfar@gmail.com>
2023-04-10 17:04:53 -0700

    fixed create spot not rendering when not logged in

--- frontend/src/components/Navigation/OpenModalMenuItem.js
+++ frontend/src/components/Navigation/OpenModalMenuItem.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useModal } from '../../context/Modal';
+import './Navigation.css';
 
 function OpenModalMenuItem({
   modalComponent, // component to render inside the modal
