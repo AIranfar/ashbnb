8056f286415b4d9ae864e9188e6bec90e3543746
Ashkaun Iranfar <ashkauniranfar@gmail.com>
2023-04-11 14:45:04 -0700

    nav bar update and css

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -9,7 +9,10 @@ function Navigation({ isLoaded }){
 
   return (
     <ul className='nav-bar'>
+      <div className='test'>
+      <img src='https://cdn.dribbble.com/users/2280052/screenshots/5337647/media/96b909cc031e4a49d2ae4692717b3320.png?compress=1&resize=400x300' alt='ash-picture' className='ash-pic' />
       <NavLink className='logo' exact to='/'>ashbnb</NavLink>
+      </div>
       <div className='create'>
         {sessionUser ? <NavLink className='create-button' to='/spots/new'>Create a New Spot</NavLink> : null}
         {isLoaded && (
